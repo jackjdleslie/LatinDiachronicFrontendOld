7e7be44d3b2e1b633d2e406fa1a2d6762492a31b
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-10-27 18:44:37 +0000

    Fake results for design

--- src/pages/results.js
+++ src/pages/results.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 
 import { Header } from '../components';
 
-import styles from './index.module.css';
+import styles from './results.module.css';
 
 export default ({
   location: {
@@ -23,14 +23,99 @@ export default ({
     }
   `;
   const [result] = useQuery(APOLLO_QUERY);
-  console.warn(result);
+  /*
+  const {
+    data: {
+      latin: {
+        lemma: { occurences },
+      },
+    },
+  } = result;
+  */
   return (
     <div className={styles.container}>
+      <Header>Latin Diachronic Analysis</Header>
       <div className={styles.main}>
-        <Header>Latin Diachronic Analysis</Header>
-        <p>{authors}</p>
-        <p>{lemma}</p>
+        <h2 className={styles.results}>
+          {fake.count} results for <span className={styles.lemma}>{lemma}</span>
+        </h2>
+        {fake.occurrences.map(occurence => (
+          <div className={styles.occurence}>
+            <h4 className={styles.occurenceSourceName}>
+              {occurence.source.name.substring(
+                0,
+                occurence.source.name.length - 4
+              )}
+            </h4>
+            <p className={styles.occurenceLine}>
+              {occurence.line.split(' ').map(word => {
+                if (word.toLowerCase() === lemma.toLowerCase()) {
+                  return <b> {word} </b>;
+                }
+                return ` ${word} `;
+              })}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
+};
+
+const fake = {
+  count: 1068,
+  occurrences: [
+    {
+      line: 'populariter loquatur, esse interdum paulo hebetiorem. sed',
+      source: {
+        name: 'De Finibus.txt',
+      },
+    },
+    {
+      line:
+        'sententiam dicere. Interdum, ut Pomponius scribit, recte nudo pacto fiet compromissum, ut',
+      source: {
+        name: 'Digesta Iustiniani.txt',
+      },
+    },
+    {
+      line:
+        'rosei coloris digitos habentem, interdum κροκόπεπλον dicit Auroram,',
+      source: {
+        name: 'In Vergilii Aeneidos Libros.txt',
+      },
+    },
+    {
+      line: 'prospicit occasus, interdum respicit ortus,',
+      source: {
+        name: 'Metamorphoses.txt',
+      },
+    },
+    {
+      line: 'interdum Eurus, quem quidam Vulturnum appellant.',
+      source: {
+        name: 'De Re Rustica.txt',
+      },
+    },
+    {
+      line:
+        'persequendarum gratia haec actio. Interdum quibusdam nec ex iustis possessionibus',
+      source: {
+        name: 'Digesta Iustiniani.txt',
+      },
+    },
+    {
+      line: 'discutitur. interdum magis prosunt gramina concisa et his',
+      source: {
+        name: 'De Re Rustica.txt',
+      },
+    },
+    {
+      line:
+        'etiam hominis commodati custodia praestetur, apud ueteres dubitatum est. nam interdum',
+      source: {
+        name: 'Digesta Iustiniani.txt',
+      },
+    },
+  ],
 };
